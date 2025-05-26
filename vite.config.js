a6c9d3e2b9a5be065b14811f9290b9df0da98532
Ugbedah Amos <ugbedahamos@gmail.com>
2025-05-26 23:37:03 +0100

    trying to troubleshoot

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,45 @@ export default defineConfig({
   plugins: [
     react({
       jsxRuntime: 'automatic',
-      // Remove the babel config since we're using automatic runtime
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', {
+            runtime: 'automatic'
+          }]
+        ]
+      }
     })
   ],
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.(jsx|tsx)?$/,
     exclude: [],
+    jsxInject: undefined // Remove automatic React injection
   },
   resolve: {
     alias: {
       '@': '/src',
     },
+    extensions: ['.js', '.jsx', '.json'] // Add this line
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
   },
   optimizeDeps: {
+    include: ['react', 'react-dom'],
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+        '.js': 'jsx'
+      }
+    }
+  }
 });
